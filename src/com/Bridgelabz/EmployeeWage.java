556b16cae77babb4acca5a80d83f27ed1b2aacbe
Kiranm1803 <kiranmundhe1803@gmail.com>
2023-02-06 01:42:27 -0800

    Added UC6 : Calculate wage till a condition of total working hours is reached for a month.

--- src/com/Bridgelabz/EmployeeWage.java
+++ src/com/Bridgelabz/EmployeeWage.java
@@ -7,34 +7,39 @@ public class EmployeeWage {
 	public static final int IS_FULL_TIME = 2;
 	public static final int EMP_RATE_PER_HOUR = 20;
 	public static final int NUM_OF_WORKING_DAYS = 2;
+	public static final int MAX_HRS_IN_MONTH = 10;
 
 	public static void main(String[] args) {
 	
 	 //variables
-	 int empHrs = 0;
-	 int empWage = 0;
+	
+	 int totalEmpHrs = 0;
 	 int totalEmpWage=0;
-	 
+	 int totalWorkingDay=0;
 	 //computation
 	
-	 for(int day=0;day<NUM_OF_WORKING_DAYS;day++)
-	 {
+	while(totalEmpHrs <= MAX_HRS_IN_MONTH && totalWorkingDay < NUM_OF_WORKING_DAYS)
+	{
+		 totalWorkingDay++;
+		 
+	  int empHrs = 0;
 	  int empCheck = (int)Math.floor(Math.random() * 10) % 3;
 	 
 	  switch(empCheck) {
 	 
-	 case IS_FULL_TIME:
+	  case IS_FULL_TIME:
 	           empHrs = 8;
 	           break;
-	 case IS_PART_TIME:
+	  case IS_PART_TIME:
 	           empHrs = 4;
 	           break;
-	 default :
+	  default :
 	           empHrs = 0;
       }
 	 
-	 empWage= empHrs * EMP_RATE_PER_HOUR;
-	 totalEmpWage +=empWage;
+	 totalEmpHrs += empHrs;
+	 int empWage = empHrs * EMP_RATE_PER_HOUR;
+	 totalEmpWage += empWage;
 	 System.out.println("Employee Wage: "+empWage);
 	}
 	 System.out.println("Total Employee Wage: "+totalEmpWage);
